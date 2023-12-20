f65b39b6a381aef50e2390eea3901427137984f2
dylaneeyalla <120025527+dylaneeyalla@users.noreply.github.com>
2023-12-21 00:06:32 +0100

    Add files via upload

---
+++ cathégo.js
@@ -0,0 +1,18 @@
+export const categories = [
+    {
+        id: 1,
+        title:"Fruit",
+    },
+    {
+        id: 2,
+        title:"legumes",
+    },
+    {
+        id: 3,
+        title:"boisson",
+    },
+    {
+        id: 3,
+        title:"boisson",
+    },
+]
